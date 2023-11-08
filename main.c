6c610b5f08b03b65616be8779d7e231354c6a0de
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-11-08 10:45:01 +0100

    feat(main): Implement solution from A. Pfeiffer

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
+// Lösung von Andreas Pfeiffer
+
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World!\n");
+
+    int jahr = 2024;
+
+    if (jahr % 4 == 0) {
+        if (jahr % 100 == 0) {
+            if (jahr % 400 == 0) {
+                printf("%d ist ein Schaltjahr.\n", jahr);
+            } else {
+                printf("%d ist kein Schaltjahr.\n", jahr);
+            }
+        } else {
+            printf("%d ist ein Schaltjahr.\n", jahr);
+        }
+    } else {
+        printf("%d ist kein Schaltjahr.\n", jahr);
+    }
+
     return 0;
 }
+
+
+// #include <stdio.h>
+
+//int main() {
+//    int jahr;
+
+//    printf("Bitte geben Sie eine Jahreszahl ein: ");
+//    scanf("%d", &jahr);
+
+//    if ((jahr % 4 == 0 && jahr % 100 != 0) || (jahr % 400 == 0)) {
+//        printf("%d ist ein Schaltjahr.\n", jahr);
+//    } else {
+//        printf("%d ist kein Schaltjahr.\n", jahr);
+//    }
+
+//    return 0;
+//}
+
